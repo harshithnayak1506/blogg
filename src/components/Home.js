803cbda731ac4579485bebd7cd7b2634ac099cf0
harshithnayak1506 <164065736+harshithnayak1506@users.noreply.github.com>
2024-05-21 17:58:57 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import mycss from './Home.css'
+function Home() {
+  return (
+    <div>
+     <p>
+     A book is a medium for recording information in the form of writing or images. Books are typically composed of many pages, bound together and protected by a cover.[1] Modern bound books were preceded by many other written mediums, such as the codex and the scroll. The book publishing process is the series of steps involved in their creation and dissemination. 
+     </p>
+    </div>
+  )
+}
+
+export default Home
